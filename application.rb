2b51c223a685cc50c8e4872eb311805c2390f542
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-21 23:44:09 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World! My name is Yong-Nicholas."
+    resp.write "Hello, my name is Yong-Nicholas."
     resp.finish
   end
 
