91398f8eb74e41a228881a65bc4dfdc5d4a4b52f
Ahamed <ahamedali95@gmail.com>
2018-04-23 10:13:49 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,13 @@
 class Application
-
+  #All this method needs to do is return a response which consists of the status code, any headers, and the body.
   def call(env)
+    # We first create a Rack::Response object, then add some text "Hello, World" to the body,
+    # and complete the response with the #finish method. By default, Rack sets our status codes and headers.
+    # Don't worry about the env input.
+    # This holds all of the request info in it and we will use it later!
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Ahamed"
     resp.finish
   end
 
 end
-
